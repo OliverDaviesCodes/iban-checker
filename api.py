859ab4129251f7aa515d612d393349dd1b08b14d
OliverDaviesCodes <oliverdavieswork@gmail.com>
2022-04-05 07:21:18 +0000

    Built api.py to validate iban

---
+++ api.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify, render_template
+from iban_checker import iban_is_valid
+
+app = Flask(__name__)
+
+
+@app.route('/validate-iban', methods=['POST'])
+def validate_iban():
+    """  """
+    data = request.json
+    iban = data.get("iban")
+
+    if iban == None:
+        return jsonify(statusCode=400, message="IBAN number must be included")
+
+    valid = iban_is_valid(iban)
+    print(valid)   
+    return jsonify(statusCode=200, valid=valid)
+
+
